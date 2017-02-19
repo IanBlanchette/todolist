7c4447dbe077e78a2c2b681164f6b9cfd9740465
Ian <ianjblanchette@gmail.com>
2017-02-18 20:51:21 -0500

    trying to get the database to connect

--- home.php
+++ home.php
@@ -7,8 +7,23 @@ if(!isset($_SESSION['admin']))
 }
 
 
-?>
 
+$dsn = 'mysql:host=ca-cdbr-azure-central-a.cloudapp.net;dbname=todolist';
+$username = 'bdeed905f38b4b';
+$password = 'd78d2897'; 
+
+
+try 
+{
+  $db= new PDO($dsn, $username, $password); // creates PDQ object
+  echo '<p>You are connected to the database</p>';
+}
+catch 
+{
+  $error_message = e->getMessage();
+  echo '<p>An error occurred while connecting to the database: $error_message </p>'
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
