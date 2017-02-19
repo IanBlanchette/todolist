5f881a437c4536b7e39108f4f2e13893e01c3d71
Ian <ianjblanchette@gmail.com>
2017-02-18 21:01:03 -0500

    working on connection string

--- home.php
+++ home.php
@@ -8,7 +8,7 @@ if(!isset($_SESSION['admin']))
 
 
 
-$dsn = 'mysql:host=ca-cdbr-azure-central-a.cloudapp.net;dbname=todolist';
+$dsn = 'mysql:ca-cdbr-azure-central-a.cloudapp.net;dbname=todolist';
 $username = 'bdeed905f38b4b';
 $password = 'd78d2897'; 
 
