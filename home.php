1223a7ba67c1915b4db4517f15fd4b5403474dad
Ian <ianjblanchette@gmail.com>
2017-02-18 20:59:35 -0500

    added a line to see if the database would connect

--- home.php
+++ home.php
@@ -12,7 +12,7 @@ $dsn = 'mysql:host=ca-cdbr-azure-central-a.cloudapp.net;dbname=todolist';
 $username = 'bdeed905f38b4b';
 $password = 'd78d2897'; 
 
-
+$db= new PDO($dsn, $username, $password); // creates PDQ object
 try 
 {
   $db= new PDO($dsn, $username, $password); // creates PDQ object
