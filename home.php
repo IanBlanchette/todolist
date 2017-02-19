83638eedb693fa42d839913026df7e7c0c133a9b
Ian <ianjblanchette@gmail.com>
2017-02-18 21:07:28 -0500

    got the string to figure out what the problem was

--- home.php
+++ home.php
@@ -8,11 +8,11 @@ if(!isset($_SESSION['admin']))
 
 
 
-$dsn = 'mysql:ca-cdbr-azure-central-a.cloudapp.net;dbname=todolist';
+$dsn = 'mysql:host=ca-cdbr-azure-central-a.cloudapp.net;dbname=todolist';
 $username = 'bdeed905f38b4b';
 $password = 'd78d2897'; 
 
-$db= new PDO($dsn, $username, $password); // creates PDQ object
+
 try 
 {
   $db= new PDO($dsn, $username, $password); // creates PDQ object
@@ -21,7 +21,7 @@ try
 catch (PDOException $e)
 {
   $error_message = $e->getMessage();
-  echo '<p>An error occurred while connecting to the database</p>';
+  echo '<p>An error occurred while connecting to the database: ' . $error_message . '</p>';
 }
 ?>
 
