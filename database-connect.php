a353fb48f313ce3b5ef6dd23f92c403c2eaed319
Ian <ianjblanchette@gmail.com>
2017-02-20 13:18:29 -0500

    wrong log in info

--- database-connect.php
+++ database-connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$dsn = 'mysql:host=ca-cdbr-azure-central-a.cloudapp.net;dbname=todolist';
-$username = 'bdeed905f38b4b';
-$password = 'd78d2897';   
+$dsn = 'mysql:host=ca-cdbr-azure-central-a.cloudapp.net;dbname=ian_todolist';
+$username = 'b78671959fb3a5';
+$password = '49314434';   
 
 try 
 {
